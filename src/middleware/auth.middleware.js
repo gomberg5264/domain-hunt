bab2f4871093af2e636bdc3914017b49b054020e
Shriram Balaji <>
2018-04-23 00:50:41 +0530

    Using prod config for auth

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,7 +1,7 @@
 "use strict";
 const path = require("path");
 const HOMEDIR = path.join(__dirname, "..");
-const config = require(path.join(HOMEDIR, "config", "local.config"));
+const config = require(path.join(HOMEDIR, "config", "prod.config"));
 const ACCESS_KEY = process.env.ACCESS_KEY || config.api.goDaddy.key;
 const SECRET = process.env.SECRET || config.api.goDaddy.secret;
 const API_PATH = process.env.API_PATH || config.api.goDaddy.path;

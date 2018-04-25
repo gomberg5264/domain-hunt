75ad7b336d7c455e73ee93f205c951195f78366c
Shriram Balaji <>
2018-04-25 11:00:28 +0530

    Adding /ping integration test

--- test/integration/routes/routes.integration.js
+++ test/integration/routes/routes.integration.js
@@ -6,11 +6,18 @@ const SRCDIR = path.join(HOMEDIR, "src");
 const baseSettings = require(path.join(HOMEDIR, "test", "_base"));
 const { error, debug, success } = require("util-box");
 
+test("GET /ping", async t => {
+	t.plan(2);
+	const response = await request(baseSettings.BASE_URL).get("/ping");
+	t.is(response.status, 200);
+	t.is(response.body.message, "pong");
+});
+
 test("GET /api", async t => {
 	t.plan(2);
 	const response = await request(baseSettings.BASE_URL).get("/api");
 	t.is(response.status, 200);
-	t.is(response.body.message, "Welcome to Domain Hunt! 🌎");
+	t.is(response.body.message, "Welcome to Domain Hunt's RESTful API 🌎");
 });
 
 test("GET /api/available", async t => {
